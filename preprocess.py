6a1802c4bfb99cc2b2b325e3aec9b15baa362cd9
senthilvel2002 <senthilv979@gmail.com>
2025-06-27 17:27:21 +0530

    commit

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,20 @@
 import nltk
+import string
+from nltk.corpus import stopwords
+
+# Ensure stopwords are downloaded
 try:
     nltk.data.find('corpora/stopwords')
 except LookupError:
     nltk.download('stopwords')
+
+def clean_text(text):
+    # Lowercase
+    text = text.lower()
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = text.split()
+    words = [word for word in words if word not in stop_words]
+    return ' '.join(words)
